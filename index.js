b3bab7bd5c21c8bacdb6faa5a35d573028c844aa
rabinzon <ildarrazin@gmail.com>
2020-01-25 19:29:00 +0300

    fix

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require('express');
 
 const feed = new Podcast({
   title: 'Перевод Корана',
-  author: 'Перевод Эльмира Кулиева.',
   description: 'Смысловой перевод Священного Корана на русский язык. Перевод Эльмира Кулиева.',
   feed_url: 'http://f2f3dff6-e88d-4558-950d-621650c19dd1.pub.cloud.scaleway.com:6346/rss.xml',
   site_url: 'http://f2f3dff6-e88d-4558-950d-621650c19dd1.pub.cloud.scaleway.com:6346/',
+  image_url: "https://bts.s3.nl-ams.scw.cloud/quran-ru/img.jpg",
   language: 'ru',
   categories: ['Religion & Spirituality/Islam'],
   pubDate: 'Mon, 20 Jan 2020 06:38:17 +0000',
